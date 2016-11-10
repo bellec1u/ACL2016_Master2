fc2bf9f34e297c6e029cba1f4bf5e162431be7d6
Pierre-Marie Junges <junges1u@fst-ats-309-l01.fst.site.univ-lorraine.fr>
2016-11-10 11:12:09 +0100

    [ADD] classes

---
+++ src/game/element/BasicLaser.java
@@ -0,0 +1,14 @@
+package game.element;
+
+/**
+ * Project "Space Invader"
+ * M1 Informatique 2016/2017
+ * @author BELLEC Leopold
+ * @author DAUZVARDIS Juozas
+ * @author JUNGES Pierre-Marie
+ * @author LIPSKI Guillaume
+ *
+ */
+public class BasicLaser extends Laser{
+
+}
