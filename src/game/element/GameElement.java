fd3784d6a76239ec32c368c0488bae725291aeae
Guillaume Lipski <lipski1u@fst-ats-309-l03.fst.site.univ-lorraine.fr>
2016-11-10 11:53:20 +0100

    [EDT] classe GameElement

--- src/game/element/GameElement.java
+++ src/game/element/GameElement.java
@@ -1,5 +1,7 @@
 package game.element;
 
+import game.World;
+
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
@@ -50,11 +52,14 @@ public abstract class GameElement {
 	 * @param y position dans l'axe Y
 	 */
 	public void setPosition(double x, double y){
-		if(x>0&&y>0){
+		if(x>0 && y>0 && x<World.WIDTH && y<World.HEIGHT){
 			position.setLocation(x, y);
 		}
 	}
 	
+	/**
+	 * donne le nom de l'element et sa position
+	 */
 	public String toString(){
 		String s=this.toString()+" en position : "+this.position.getX()+", "+this.position.getY()+".\n";
 		return s;
