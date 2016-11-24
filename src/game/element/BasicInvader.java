8e20329023e5b1eb227b54807d0b20a6ca84905c
bellec1u <leopold.bellec@gmail.com>
2016-11-24 18:27:45 +0100

    [ADD] choix aléatoire parmis 3 types de skin pour les basicInvaders

--- src/game/element/BasicInvader.java
+++ src/game/element/BasicInvader.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.Random;
 
 /**
  * Project "Space Invader"
@@ -22,15 +23,18 @@ public class BasicInvader extends Invader {
 	/** Speed of a BasicInvader **/
 	private static final double SPEED = 90.0; // 1.5 pixels every refresh call
 	private static final int TEXTURE_CHANGE_DELAY = 500;
-	
+
 	/** Dimension (width and height) **/
 	private final static int WIDTH = 24;	
 	private final static int HEIGHT = 16;
-	
+
 	/** Attributes used to manage the Element's animation **/
 	private int textureIndex;
 	private long lastTextureChange;
-	
+
+	/** determine si l'invader est de type A, B ou C **/
+	private int typeInvader;
+
 	/** Score value**/
 	public static final int SCORE = 10;
 
@@ -43,18 +47,27 @@ public class BasicInvader extends Invader {
 		this.turnDown();
 		this.textureIndex = 0;
 		this.lastTextureChange = System.currentTimeMillis();
+		Random r = new Random();
+		this.typeInvader = r.nextInt(3);
 	}
-	
+
 	/**
 	 * Returns Image[] of a BasicInvader
 	 */
 	@Override
-    public Image[] getTexture() {
-        return TextureFactory.getInstance().getInvaderAImg();
-    }
-	
+	public Image[] getTexture() {
+		switch (this.typeInvader) {
+		case 0 :
+			return TextureFactory.getInstance().getInvaderAImg();
+		case 1 :
+			return TextureFactory.getInstance().getInvaderBImg();
+		default :
+			return TextureFactory.getInstance().getInvaderCImg();
+		}
+	}
+
 	public boolean isOutOfScreen() {
-	    return position.getY() > World.HEIGHT;
+		return position.getY() > World.HEIGHT;
 	}
 
 	/**
@@ -64,13 +77,13 @@ public class BasicInvader extends Invader {
 	@Override
 	public void update(double delta) {
 		move(delta);
-		
+
 		/*
 		 * Change the indice without using a Timer
 		 */
 		if (System.currentTimeMillis() > lastTextureChange + TEXTURE_CHANGE_DELAY) {
-		    textureIndex = ++textureIndex % getTexture().length;
-		    lastTextureChange = System.currentTimeMillis();
+			textureIndex = ++textureIndex % getTexture().length;
+			lastTextureChange = System.currentTimeMillis();
 		}
 	}
 
